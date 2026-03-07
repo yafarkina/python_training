8a80b409bc12eb5ce65d02a5085e75564f2c5373
Yafarkina Natalia <yafarkina@gmail.com>
2026-03-08 00:15:43 +0400

    добавлен черновик теста создания группы

---
+++ test_add_group.py
@@ -0,0 +1,53 @@
+import ...
+
+class TestAddGroup(unittest.TestCase):
+    def setUp(self):
+        self.wd = webdriver.Chrome()
+        self.wd.implicitly_wait(30)
+
+    
+    def test_add_group(self):
+        wd = self.wd
+        wd.get("http://localhost/addressbook/group.php")
+        wd.find_element_by_xpath("//*/text()[normalize-space(.)='']/parent::*").click()
+        wd.find_element_by_name("user").click()
+        wd.find_element_by_name("user").clear()
+        wd.find_element_by_name("user").send_keys("admin")
+        wd.find_element_by_name("pass").click()
+        wd.find_element_by_name("pass").clear()
+        wd.find_element_by_name("pass").send_keys("secret")
+        wd.find_element_by_xpath("//input[@value='Login']").click()
+        wd.get("https://pythonru.com/osnovy/modul-math-python")
+        wd.get("http://localhost/addressbook/group.php")
+        wd.find_element_by_name("new").click()
+        wd.get("https://pythonru.com/osnovy/modul-math-python")
+        wd.get("http://localhost/addressbook/group.php?new=New+group")
+        wd.find_element_by_name("group_name").click()
+        wd.find_element_by_name("group_name").clear()
+        wd.find_element_by_name("group_name").send_keys("test")
+        wd.find_element_by_name("group_header").click()
+        wd.find_element_by_name("group_header").clear()
+        wd.find_element_by_name("group_header").send_keys("test")
+        wd.find_element_by_name("group_footer").click()
+        wd.find_element_by_name("group_footer").clear()
+        wd.find_element_by_name("group_footer").send_keys("test")
+        wd.find_element_by_name("submit").click()
+        wd.get("http://localhost/addressbook/group.php")
+        wd.find_element_by_link_text("group page").click()
+        wd.find_element_by_link_text("Logout").click()
+    
+    def is_element_present(self, how, what):
+        try: self.wd.find_element(by=how, value=what)
+        except NoSuchElementException as e: return False
+        return True
+    
+    def is_alert_present(self):
+        try: self.wd.switch_to_alert()
+        except NoAlertPresentException as e: return False
+        return True
+    
+    def tearDown(self):
+        self.wd.quit()
+
+if __name__ == "__main__":
+    unittest.main()
