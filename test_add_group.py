1fae154adc52c9185929f82c777b54d9af0889d4
Yafarkina Natalia <yafarkina@gmail.com>
2026-03-08 23:42:21 +0400

    доработан тест

--- test_add_group.py
+++ test_add_group.py
@@ -1,14 +1,18 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import Select
-from selenium.common.exceptions import NoSuchElementException
-from selenium.common.exceptions import NoAlertPresentException
-import unittest, time, re
+#  -*- coding: utf-8 -*-
 
-class TestAddGroup(unittest.TestCase):
+from selenium.webdriver.firefox.webdriver import WebDriver
+#from selenium.webdriver.common.action_chains import ActionChains
+#from selenium.webdriver.common.keys import Keys
+#from selenium.webdriver.support.ui import Select
+#from selenium.common.exceptions import NoSuchElementException
+#from selenium.common.exceptions import NoAlertPresentException
+import unittest
+#import time
+
+class test_add_group(unittest.TestCase):
     def setUp(self):
-        self.wd = webdriver.Chrome()
+        self.wd = WebDriver(executable_path="drivers/geckodriver.exe")
+        #self.wd = webdriver.Firefox()
         self.wd.implicitly_wait(30)
 
     
@@ -22,7 +26,7 @@ class TestAddGroup(unittest.TestCase):
         wd.find_element_by_name("pass").clear()
         wd.find_element_by_name("pass").send_keys("secret")
         wd.find_element_by_css_selector("input[type=\"submit\"]").click()
-        wd.find_element_by_link_text("group").click()
+        wd.find_element_by_link_text("groups").click()
         wd.find_element_by_name("new").click()
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
@@ -37,15 +41,15 @@ class TestAddGroup(unittest.TestCase):
         wd.find_element_by_link_text("group page").click()
         wd.find_element_by_link_text("Logout").click()
     
-    def is_element_present(self, how, what):
-        try: self.wd.find_element(by=how, value=what)
-        except NoSuchElementException as e: return False
-        return True
+  #  def is_element_present(self, how, what):
+  #     try: self.wd.find_element(by=how, value=what)
+  #     except NoSuchElementException as e: return False
+  #     return True
     
-    def is_alert_present(self):
-        try: self.wd.switch_to_alert()
-        except NoAlertPresentException as e: return False
-        return True
+ #   def is_alert_present(self):
+ #       try: self.wd.switch_to_alert()
+ #       except NoAlertPresentException as e: return False
+ #       return True
     
     def tearDown(self):
         self.wd.quit()
