9abba98d287bdf9c4fc8a14b7266923a777f97f1
Yafarkina Natalia <yafarkina@gmail.com>
2026-03-09 21:06:50 +0400

    добавлен файл conftest

--- fixture/application.py
+++ fixture/application.py
@@ -1,20 +1,17 @@
 from selenium.webdriver.firefox.webdriver import WebDriver
-
-from fixture.contact import ContactHelper
 from fixture.session import SessionHelper
 from fixture.group import GroupHelper
 from fixture.contact import ContactHelper
 
 class Application:
     def __init__(self):
-        #self.wd = WebDriver(executable_path="../drivers/geckodriver.exe")
+       # self.wd = WebDriver(executable_path="../drivers/geckodriver.exe")
         self.wd = WebDriver(executable_path="C:\PythonProject\drivers\geckodriver.exe")
        # self.wd = webdriver.Firefox()
-        self.wd.implicitly_wait(30)
+        self.wd.implicitly_wait(50)
         self.session= SessionHelper(self)
         self.group = GroupHelper(self)
         self.contact = ContactHelper(self)
-
 
 
     def destroy(self):
