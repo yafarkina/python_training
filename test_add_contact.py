ff16813225f279bd31bab2138a47819a699a9fcc
Yafarkina Natalia <yafarkina@gmail.com>
2026-03-09 12:10:48 +0400

    добавлен тест для страницы контактов

---
+++ test_add_contact.py
@@ -0,0 +1,60 @@
+#  -*- coding: utf-8 -*-
+
+from selenium.webdriver.firefox.webdriver import WebDriver
+import unittest
+
+
+class test_add_contact(unittest.TestCase):
+    def setUp(self):
+        self.wd = WebDriver(executable_path="drivers/geckodriver.exe")
+        # self.wd = webdriver.Firefox()
+        self.wd.implicitly_wait(30)
+
+    def test_add_group(self):
+        wd = self.wd
+        # open home page
+        wd.get("http://localhost/addressbook/")
+        # login
+        wd.find_element_by_name("user").click()
+        wd.find_element_by_name("user").clear()
+        wd.find_element_by_name("user").send_keys("admin")
+        wd.find_element_by_name("pass").click()
+        wd.find_element_by_name("pass").clear()
+        wd.find_element_by_name("pass").send_keys("secret")
+        wd.find_element_by_css_selector("input[type=\"submit\"]").click()
+        # open contact page
+        wd.find_element_by_link_text("add new").click()
+        # fill contact form
+        wd.find_element_by_name("firstname").click()
+        wd.find_element_by_name("firstname").clear()
+        wd.find_element_by_name("firstname").send_keys("first")
+        wd.find_element_by_name("lastname").click()
+        wd.find_element_by_name("lastname").clear()
+        wd.find_element_by_name("lastname").send_keys("last")
+        wd.find_element_by_name("company").click()
+        wd.find_element_by_name("company").clear()
+        wd.find_element_by_name("company").send_keys("company")
+        wd.find_element_by_name("address").click()
+        wd.find_element_by_name("address").clear()
+        wd.find_element_by_name("address").send_keys("address")
+        wd.find_element_by_name("home").click()
+        wd.find_element_by_name("home").clear()
+        wd.find_element_by_name("home").send_keys("home")
+        # submit contact creation
+        wd.find_element_by_name("submit").click()
+        # return to home page
+        wd.find_element_by_link_text("home page").click()
+        # logout
+        wd.find_element_by_link_text("Logout").click()
+
+    def tearDown(self):
+        self.wd.quit()
+
+if __name__ == "__main__":
+    unittest.main()
+
+
+
+
+
+
