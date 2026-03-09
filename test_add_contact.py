ad1bd6c771b6e0a673a91ed2cfb589d0cfba942e
Yafarkina Natalia <yafarkina@gmail.com>
2026-03-09 12:45:21 +0400

    выполнен рефакторинг, добавлены методы в классе создания контакта

--- test_add_contact.py
+++ test_add_contact.py
@@ -12,18 +12,22 @@ class test_add_contact(unittest.TestCase):
 
     def test_add_group(self):
         wd = self.wd
-        # open home page
-        wd.get("http://localhost/addressbook/")
-        # login
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys("admin")
-        wd.find_element_by_name("pass").click()
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys("secret")
-        wd.find_element_by_css_selector("input[type=\"submit\"]").click()
-        # open contact page
-        wd.find_element_by_link_text("add new").click()
+        self.open_home_page(wd)
+        self.login(wd)
+        self.open_contact_page(wd)
+        self.create_contact(wd)
+        self.return_to_home_page(wd)
+        self.logout(wd)
+
+    def logout(self, wd: WebDriver):
+        # logout
+        wd.find_element_by_link_text("Logout").click()
+
+    def return_to_home_page(self, wd: WebDriver):
+        # return to home page
+        wd.find_element_by_link_text("home page").click()
+
+    def create_contact(self, wd: WebDriver):
         # fill contact form
         wd.find_element_by_name("firstname").click()
         wd.find_element_by_name("firstname").clear()
@@ -42,10 +46,24 @@ class test_add_contact(unittest.TestCase):
         wd.find_element_by_name("home").send_keys("home")
         # submit contact creation
         wd.find_element_by_name("submit").click()
-        # return to home page
-        wd.find_element_by_link_text("home page").click()
-        # logout
-        wd.find_element_by_link_text("Logout").click()
+
+    def open_contact_page(self, wd: WebDriver):
+        # open contact page
+        wd.find_element_by_link_text("add new").click()
+
+    def login(self, wd: WebDriver):
+        # login
+        wd.find_element_by_name("user").click()
+        wd.find_element_by_name("user").clear()
+        wd.find_element_by_name("user").send_keys("admin")
+        wd.find_element_by_name("pass").click()
+        wd.find_element_by_name("pass").clear()
+        wd.find_element_by_name("pass").send_keys("secret")
+        wd.find_element_by_css_selector("input[type=\"submit\"]").click()
+
+    def open_home_page(self, wd: WebDriver):
+        # open home page
+        wd.get("http://localhost/addressbook/")
 
     def tearDown(self):
         self.wd.quit()
