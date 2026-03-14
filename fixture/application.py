68f2550b1427c2f21d0e9a356cf7de85f63869e1
Yafarkina Natalia <yafarkina@gmail.com>
2026-03-15 00:28:19 +0400

    добавлена проверка состояния сессии

--- fixture/application.py
+++ fixture/application.py
@@ -5,10 +5,10 @@ from fixture.contact import ContactHelper
 
 class Application:
     def __init__(self):
-        self.wd = WebDriver(executable_path="../drivers/geckodriver.exe")
-       # self.wd = WebDriver(executable_path="C:\PythonProject\drivers\geckodriver.exe")
+       # self.wd = WebDriver(executable_path="../drivers/geckodriver.exe")
+        self.wd = WebDriver(executable_path="C:\PythonProject\drivers\geckodriver.exe")
        # self.wd = webdriver.Firefox()
-        self.wd.implicitly_wait(50)
+        self.wd.implicitly_wait(10)
         self.session= SessionHelper(self)
         self.group = GroupHelper(self)
         self.contact = ContactHelper(self)
--- fixture/session.py
+++ fixture/session.py
@@ -22,3 +22,28 @@ class SessionHelper:
         wd = self.app.wd
         # logout
         wd.find_element_by_link_text("Logout").click()
+
+    def ensure_logout(self):
+        wd = self.app.wd
+        if self.is_logged_in():
+           self.logout()
+
+    def is_logged_in(self) -> bool:
+        wd = self.app.wd
+        return len(wd.find_elements_by_link_text("Logout")) > 0
+
+    def is_logged_in_as(self, username) -> bool:
+        wd = self.app.wd
+        return wd.find_element_by_xpath("//div/div[1]/form/b").text == "("+username+")"
+
+    def ensure_login(self, username, password):
+        wd = self.app.wd
+        if self.is_logged_in():
+           if self.is_logged_in_as(username):
+              return
+           else:
+                self.logout()
+        self.login(username, password)
+
+
+
