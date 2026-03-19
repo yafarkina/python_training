5deff1a67d7300ba5f331ba8a620545bfe6946f9
Yafarkina Natalia <yafarkina@gmail.com>
2026-03-19 16:58:22 +0400

    добавлена проверка наличия группы перед удалением и редактированием

--- test/test_del_group.py
+++ test/test_del_group.py
@@ -3,5 +3,7 @@
 from model.group import Group
 
 def test_delete_first_group(app):
+    if app.group.count() == 0:
+        app.group.create(Group(name="test_new", header ="test_new", footer ="test_new"))
     app.group.delete_first_group()
 
--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -3,4 +3,7 @@
 from model.group import Group
 
 def test_edit_first_group(app):
+    if app.group.count() == 0:
+       app.group.create(Group(name="test_new", header ="test_new", footer ="test_new"))
     app.group.edit_first_group(Group(name ="test_edit", header ="test_edit", footer ="test_edit"))
+
