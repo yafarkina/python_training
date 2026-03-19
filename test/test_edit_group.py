d06746a3564da538200039fb54b916c2e7027b84
Yafarkina Natalia <yafarkina@gmail.com>
2026-03-19 20:21:07 +0400

    добавлены сравнения длины списков элементов групп

--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -5,5 +5,7 @@ from model.group import Group
 def test_edit_first_group(app):
     if app.group.count() == 0:
        app.group.create(Group(name="test_new", header ="test_new", footer ="test_new"))
+    old_groups = app.group.get_group_list()
     app.group.edit_first_group(Group(name ="test_edit", header ="test_edit", footer ="test_edit"))
-
+    new_groups = app.group.get_group_list()
+    assert len(old_groups)  == len(new_groups)
