af8a9ee9bbe8b588a1d1f48e9a6eec0f3e2f32f2
Yafarkina Natalia <yafarkina@gmail.com>
2026-03-20 22:34:33 +0400

    добавлена проверка равенства объектов и списков при создании групп и контактов

--- model/contact.py
+++ model/contact.py
@@ -1,3 +1,5 @@
+from sys import maxsize
+
 class Contact:
    def __init__(self, firstname =None, lastname = None, company = None, address = None, telephone = None, id = None):
        self.firstname = firstname
@@ -11,4 +13,10 @@ class Contact:
        return "%s:%s" % (self.id, self.firstname)
 
    def __eq__(self, other):
-       return self.id == other.id and self.firstname == other.firstname
+       return (self.id is None or other.id is None or self.id == other.id) and self.firstname == other.firstname
+
+   def id_or_max(self):
+       if self.id:
+           return int(self.id)
+       else:
+            return maxsize
--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -4,9 +4,13 @@ from model.contact import Contact
 
 def test_add_contact(app):
     old_contacts = app.contact.get_contact_list()
-    app.contact.create(Contact(firstname ="first", lastname ="last", company ="company", address ="address", telephone ="home"))
+    contact = Contact(firstname ="first", lastname ="last", company ="company", address ="address", telephone ="home")
+    app.contact.create(contact)
     new_contacts = app.contact.get_contact_list()
     assert len(old_contacts) + 1 == len(new_contacts)
+    old_contacts.append(contact)
+    assert sorted(old_contacts, key = Contact.id_or_max) == sorted(new_contacts, key = Contact.id_or_max)
+
 
 
 
