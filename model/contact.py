106a6613dcea9e83f2575e841a1dcaa93251ebd9
Yafarkina Natalia <yafarkina@gmail.com>
2026-03-20 17:51:53 +0400

    добавлена проверка равенства объектов и списков при удалении групп и контаков

--- model/contact.py
+++ model/contact.py
@@ -7,3 +7,8 @@ class Contact:
        self.telephone = telephone
        self.id = id
 
+   def __repr__(self):
+       return "%s:%s" % (self.id, self.firstname)
+
+   def __eq__(self, other):
+       return self.id == other.id and self.firstname == other.firstname
