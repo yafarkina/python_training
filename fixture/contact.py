bf62382184c9b6c8e633c2e6e3cb5c69abf34fe0
Yafarkina Natalia <yafarkina@gmail.com>
2026-03-20 23:42:59 +0400

    добавлено кеширование для групп и контактов

--- fixture/contact.py
+++ fixture/contact.py
@@ -28,6 +28,7 @@ class ContactHelper:
         # submit contact creation
         wd.find_element_by_name("submit").click()
         self.return_to_home_page()
+        self.contact_cache = None
 
     def delete_first_contact(self):
         wd = self.app.wd
@@ -35,6 +36,7 @@ class ContactHelper:
         self.select_first_contact()
         wd.find_element_by_name("delete").click()
         self.return_to_home_page()
+        self.contact_cache = None
 
     def edit_first_contact(self, contact):
         wd = self.app.wd
@@ -44,6 +46,7 @@ class ContactHelper:
         self.fill_contact_form(contact)
         wd.find_element_by_name("update").click()
         self.return_to_home_page()
+        self.contact_cache = None
 
     def select_first_contact(self):
         wd = self.app.wd
@@ -68,13 +71,16 @@ class ContactHelper:
         wd = self.app.wd
         return len(wd.find_elements_by_name("selected[]"))
 
+    contact_cache = None
+
     def get_contact_list(self):
-        wd = self.app.wd
-        contacts = []
-        for element in wd.find_elements_by_css_selector("tr[name=\"entry\"]"):
-        #for element in wd.find_elements_by_css_selector("tr.entry"):
-            cell = element.find_elements_by_tag_name("td")
-            firstname = cell[2].text
-            id = element.find_element_by_name("selected[]").get_attribute("value")
-            contacts.append(Contact(firstname = firstname, id = id))
-        return contacts
+        if self.contact_cache is None:
+            wd = self.app.wd
+            self.contact_cache = []
+            for element in wd.find_elements_by_css_selector("tr[name=\"entry\"]"):
+            #for element in wd.find_elements_by_css_selector("tr.entry"):
+                cell = element.find_elements_by_tag_name("td")
+                firstname = cell[2].text
+                id = element.find_element_by_name("selected[]").get_attribute("value")
+                self.contact_cache.append(Contact(firstname = firstname, id = id))
+        return list(self.contact_cache)
--- fixture/group.py
+++ fixture/group.py
@@ -43,6 +43,7 @@ class GroupHelper:
         # submit group creation
         wd.find_element_by_name("submit").click()
         self.return_to_group_page()
+        self.group_cache = None
 
     def delete_first_group(self):
         wd = self.app.wd
@@ -51,6 +52,7 @@ class GroupHelper:
         # submit deletion
         wd.find_element_by_name("delete").click()
         self.return_to_group_page()
+        self.group_cache = None
 
     def edit_first_group(self,group):
         wd = self.app.wd
@@ -63,6 +65,7 @@ class GroupHelper:
         #submit form
         wd.find_element_by_name("update").click()
         self.return_to_group_page()
+        self.group_cache = None
 
 
     def modify_first_group(self, new_group_data):
@@ -76,19 +79,23 @@ class GroupHelper:
         # submit form
         wd.find_element_by_name("update").click()
         self.return_to_group_page()
+        self.group_cache = None
 
     def count(self):
         wd = self.app.wd
         self.open_group_page()
         return len(wd.find_elements_by_name("selected[]"))
 
+    group_cache = None
+
     def get_group_list(self):
-        wd = self.app.wd
-        self.open_group_page()
-        groups = []
-        for element in wd.find_elements_by_css_selector("span.group"):
-            text = element.text
-            id = element.find_element_by_name("selected[]").get_attribute("value")
-            groups.append(Group(name=text, id=id))
-        return groups
+        if self.group_cache is None:
+            wd = self.app.wd
+            self.open_group_page()
+            self.group_cache = []
+            for element in wd.find_elements_by_css_selector("span.group"):
+                text = element.text
+                id = element.find_element_by_name("selected[]").get_attribute("value")
+                self.group_cache.append(Group(name=text, id=id))
+        return list(self.group_cache)
 
