31d2b5a2b3aadf2b68e1a4b4d94d20ebcaf0d4a4
Yafarkina Natalia <yafarkina@gmail.com>
2026-03-28 20:23:26 +0400

    добавлены опции командной строки c типом браузера и адресом тестируемого приложения

--- fixture/application.py
+++ fixture/application.py
@@ -1,17 +1,26 @@
-from selenium.webdriver.firefox.webdriver import WebDriver
+from selenium import webdriver
 from fixture.session import SessionHelper
 from fixture.group import GroupHelper
 from fixture.contact import ContactHelper
 
 class Application:
-    def __init__(self):
+   # def __init__(self, browser="Firefox", base_url = "http://localhost/addressbook/"):
+    def __init__(self, browser, base_url):
        # self.wd = WebDriver(executable_path="../drivers/geckodriver.exe")
-        self.wd = WebDriver(executable_path="C:\PythonProject\drivers\geckodriver.exe")
-       # self.wd = webdriver.Firefox()
-     #   self.wd.implicitly_wait(10)  если требуется поставить ожидание загрузки динамических элементов
-        self.session= SessionHelper(self)
-        self.group = GroupHelper(self)
-        self.contact = ContactHelper(self)
+       if browser == "Firefox":
+           self.wd = webdriver.Firefox(executable_path="C:\PythonProject\drivers\geckodriver.exe")
+       elif browser == "Chrome":
+            self.wd = webdriver.Chrome(executable_path="C:\PythonProject\drivers\chromedriver.exe")
+       elif browser == "Edge":
+            self.wd = webdriver.Edge(executable_path="C:\PythonProject\drivers\msedgedriver.exe")
+       else :
+            raise ValueError("Unsupported browser %s" % browser)
+       #   self.wd.implicitly_wait(10)  если требуется поставить ожидание загрузки динамических элементов
+       self.session= SessionHelper(self)
+       self.group = GroupHelper(self)
+       self.contact = ContactHelper(self)
+       self.base_url = base_url
+
 
     def is_valid(self):
         try:
@@ -19,6 +28,10 @@ class Application:
             return True
         except:
             return False
+
+    def open_home_page(self):
+        wd = self.wd
+        self.wd.get(self.base_url)
 
     def destroy(self):
         self.wd.quit()
