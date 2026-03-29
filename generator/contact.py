d9eeac976ab530f0f86ff0f6d5a6ba8c53b798a2
Yafarkina Natalia <yafarkina@gmail.com>
2026-03-29 20:12:08 +0400

    реализована генерация тестовых данных в формате json

--- generator/contact.py
+++ generator/contact.py
@@ -2,7 +2,26 @@ import random
 import string
 import os.path
 import json
+import getopt
+import sys
 from model.contact import Contact
+
+
+try:
+    opts, args = getopt.getopt(sys.argv[1:], "n:f:", ["number of contacts", "file"])
+except getopt.GetoptError as err:
+    getopt.usage()
+    sys.exit(2)
+
+n = 2
+f = "data/contacts.json"
+
+for o, a in opts:
+    if o == "-n":
+        n = int(a)
+    elif o == "-f":
+        f = a
+
 
 def random_string_fio(prefix, max_len):
     symbols = string.ascii_letters
@@ -34,10 +53,10 @@ testdata = [
             email2= random_string_email(10),
             email3= random_string_email(10)
             )
-    for i in range(3)
+    for i in range(n)
 ]
 
-file = os.path.join(os.path.dirname(os.path.abspath(__file__)), ("../data/contacts.json"))
+file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", f)
 
-with open(file, "w") as f:
-    f.write(json.dumps(testdata, default=lambda x: x.__dict__, indent=2))
+with open(file, "w") as out:
+    out.write(json.dumps(testdata, default=lambda x: x.__dict__, indent=2))
--- generator/group.py
+++ generator/group.py
@@ -2,7 +2,25 @@ import random
 import string
 import os.path
 import json
+import getopt
+import sys
 from model.group import Group
+
+
+try:
+    opts, args = getopt.getopt(sys.argv[1:], "n:f:", ["number of groups", "file"])
+except getopt.GetoptError as err:
+    getopt.usage()
+    sys.exit(2)
+
+n = 2
+f = "data/groups.json"
+
+for o, a in opts:
+    if o == "-n":
+        n = int(a)
+    elif o == "-f":
+        f = a
 
 
 def random_string(prefix, max_len):
@@ -21,12 +39,12 @@ testdata_empty = [Group(name = "", header = "", footer = "")]
 testdata_generated = [Group(name = random_string("name", 10),
            header = random_string("header", 20),
            footer = random_string("footer", 20))
-           for i in range(3)
+           for i in range(n)
         ]
 
 testdata = testdata_empty + testdata_generated
 
-file = os.path.join(os.path.dirname(os.path.abspath(__file__)), ("../data/groups.json"))
+file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", f)
 
-with open(file, "w") as f:
-    f.write(json.dumps(testdata, default=lambda x: x.__dict__, indent=2))
+with open(file, "w") as out:
+    out.write(json.dumps(testdata, default=lambda x: x.__dict__, indent=2))
