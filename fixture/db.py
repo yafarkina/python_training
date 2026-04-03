d75edda13c1493518a2ab96cb70e1ae88ee58803
Yafarkina Natalia <yafarkina@gmail.com>
2026-04-03 21:05:55 +0400

    в тестах при добавлении контактов и групп данные загружаются из бд

--- fixture/db.py
+++ fixture/db.py
@@ -9,32 +9,36 @@ class DbFixture:
         self.name = name
         self.user = user
         self.password = password
-        self.connection = pymysql.connect(host = host, database = name, user = user, password = password)
+        self.connection = pymysql.connect(host = host,
+                                          database = name,
+                                          user = user,
+                                          password = password,
+                                          autocommit = True)
 
     def get_group_list(self):
-        list=[]
+        list_group=[]
         cursor = self.connection.cursor()
         try:
             cursor.execute("select group_id, group_name, group_header, group_footer from group_list")
             for row in cursor:
                 (id, name, header, footer) = row
-                list.append(Group(id = str(id), name = name, header = header, footer = footer))
+                list_group.append(Group(id = str(id), name = name, header = header, footer = footer))
         finally:
             cursor.close()
-        return list
+        return list_group
 
 
     def get_contact_list(self):
-        list=[]
+        list_contact=[]
         cursor = self.connection.cursor()
         try:
             cursor.execute("select id, firstname, lastname, company, address, home, mobile, work, email, email2, email3 from addressbook")
             for row in cursor:
                 (id, firstname, lastname, company, address, home, mobile, work, email, email2, email3) = row
-                list.append(Contact(id = str(id), firstname = firstname, lastname = lastname, company=company, address = address, homephone = home, mobilephone = mobile, workphone= work, email = email, email2 =email2, email3= email3))
+                list_contact.append(Contact(id = str(id), firstname = firstname, lastname = lastname, company=company, address = address, homephone = home, mobilephone = mobile, workphone= work, email = email, email2 =email2, email3= email3))
         finally:
             cursor.close()
-        return list
+        return list_contact
 
     def destroy(self):
         self.connection.close()
--- test/test_add_group.py
+++ test/test_add_group.py
@@ -5,11 +5,13 @@ from model.group import Group
 
 #@pytest.mark.parametrize("group", testdata, ids=[repr(x) for x in testdata])  # один из способов параметризации
 
-def test_add_group(app, json_groups):
+def test_add_group(app, db, json_groups):
     group = json_groups
-    old_groups = app.group.get_group_list()
+   # old_groups = app.group.get_group_list()
+    old_groups = db.get_group_list()
     app.group.create(group)
-    assert len(old_groups) + 1 == app.group.count()
-    new_groups = app.group.get_group_list()
+   # assert len(old_groups) + 1 == app.group.count()
+    #new_groups = app.group.get_group_list()
+    new_groups = db.get_group_list()
     old_groups.append(group)
     assert sorted(old_groups, key = Group.id_or_max) == sorted(new_groups, key = Group.id_or_max)
