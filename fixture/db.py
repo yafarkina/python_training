73bfc2312faff1c50281d45ecc4ec2d8752747f5
Yafarkina Natalia <yafarkina@gmail.com>
2026-04-03 20:39:08 +0400

    загрузка информации из бд для групп и контактов

--- fixture/db.py
+++ fixture/db.py
@@ -1,4 +1,6 @@
 import pymysql.cursors
+from model.group import Group
+from model.contact import Contact
 
 class DbFixture:
 
@@ -8,6 +10,31 @@ class DbFixture:
         self.user = user
         self.password = password
         self.connection = pymysql.connect(host = host, database = name, user = user, password = password)
+
+    def get_group_list(self):
+        list=[]
+        cursor = self.connection.cursor()
+        try:
+            cursor.execute("select group_id, group_name, group_header, group_footer from group_list")
+            for row in cursor:
+                (id, name, header, footer) = row
+                list.append(Group(id = str(id), name = name, header = header, footer = footer))
+        finally:
+            cursor.close()
+        return list
+
+
+    def get_contact_list(self):
+        list=[]
+        cursor = self.connection.cursor()
+        try:
+            cursor.execute("select id, firstname, lastname, company, address, home, mobile, work, email, email2, email3 from addressbook")
+            for row in cursor:
+                (id, firstname, lastname, company, address, home, mobile, work, email, email2, email3) = row
+                list.append(Contact(id = str(id), firstname = firstname, lastname = lastname, company=company, address = address, homephone = home, mobilephone = mobile, workphone= work, email = email, email2 =email2, email3= email3))
+        finally:
+            cursor.close()
+        return list
 
     def destroy(self):
         self.connection.close()
